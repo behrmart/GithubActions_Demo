eeb9272a88edfc4e9dc12dd5effc77dcc1d20d14
jesualujan <lujan.oficial@gmail.com>
2024-03-13 19:26:59 -0600

    ✨ implementando código en mi aplicación

---
+++ app.js
@@ -0,0 +1,5 @@
+// MOMENT es una libreria de JavaScript y tu puedes pasar datos como fechas 
+const moment = require ('moment');
+console.log('HELLO, USING GITHUB ACTIONS AS A DEMO ')
+const date = moment().format('MMM Do YYYY, h:mm:ss a')
+console.log(date)
